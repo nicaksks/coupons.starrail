2cea090f10a20b0b1afae7b6dce7cdaa96059526
nica <100009274+nicaksks@users.noreply.github.com>
2023-05-27 07:36:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const StarRail = require("./services/StarRail")
 
 async function seele() {
   const starRail = new StarRail();
-  const seele = await starRail.getCoupons();
+  const seele = await starRail.coupons();
   console.log(`< Coupons > \n${seele.coupon}\n\n < Reward > \n${seele.reward}\n\n  < Duration > \n${seele.duration}`)
 }
 
